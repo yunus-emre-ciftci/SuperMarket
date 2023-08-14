b692e1455f8372d00b880151bd316757a399c447
Yunus Emre Çiftçi <118928402+yunus-emre-ciftci@users.noreply.github.com>
2023-08-15 00:15:25 +0300

    Stock database has been created. category, subCategory and product tables has been created.

--- src/com/util/DBDataSource.java
+++ src/com/util/DBDataSource.java
@@ -1,5 +1,21 @@
 package com.util;
 
-public class DBDataSource {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DBDataSource {
+    public static final String URL = "jdbc:postgresql://localhost:5432/Stock";
+    public static final String USER = "postgres";
+    public static final String PASSWORD = "000000";
+
+    public static Connection connect() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
